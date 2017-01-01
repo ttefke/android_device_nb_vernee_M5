c2d048a7ac7e1f635fc0c2bc17fafb220c98d02f
Alberto Guanti <albertoguanti@gmail.com>
2017-01-01 17:46:17 +0100

    mt6755-common: patches: clean-up the installation script

--- patches/install.sh
+++ patches/install.sh
@@ -12,16 +12,6 @@ for dir in $dirs ; do
 	echo " "
 done
 
-dirs="system/core"
-
-for dir in $dirs ; do
-	cd $rootdirectory
-	cd $dir
-	echo "Applying $dir patches..."
-	git apply $rootdirectory/device/cyanogen/mt6755-common/patches/$dir/*.patch
-	echo " "
-done
-
 # -----------------------------------
 echo "Changing to build directory..."
 cd $rootdirectory
