9d1b7af83ed442918c32af046dd2e9e5f5c68e72
Alberto Guanti <albertoguanti@gmail.com>
2017-06-02 00:27:06 +0200

    mt6755-common: wifi: Switch to DeckerSU's wpa_supplicant
    * that should fix libwifi-service crashes on boot

--- wpa_supplicant_8_lib/Android.mk
+++ wpa_supplicant_8_lib/Android.mk
@@ -15,9 +15,8 @@
 #
 LOCAL_PATH := $(call my-dir)
 
-##### For Google SUPPLICANT #####
-ifeq ($(MTKPATH),)
-    $(warning build BASIC wpa_supplicant)
+##### For Google SUPPLICANT
+ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_8_X)
     WPA_SUPPL_DIR = external/wpa_supplicant_8
     WPA_SRC_FILE :=
 
