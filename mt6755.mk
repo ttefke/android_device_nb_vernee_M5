ee35d0cdd739c89e937ce716a1ffdef39369f131
Tobias Tefke <tobias.tefke@gmail.com>
2017-10-14 17:32:11 +0200

    Add wifi configuration files

--- mt6755.mk
+++ mt6755.mk
@@ -122,7 +122,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/throttle.sh:system/vendor/etc/throttle.sh
 
 # Charger Mode
 PRODUCT_PACKAGES += \
