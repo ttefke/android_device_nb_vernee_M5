789852ab51245925e8c55864807661c0212d3c97
Tobias Tefke <tobias.tefke@gmail.com>
2017-10-14 14:15:40 +0200

    Add media configuration files

--- device.mk
+++ device.mk
@@ -25,6 +25,31 @@
 
 $(call inherit-product-if-exists, vendor/vernee/M5/device-vendor.mk)
 
+# Inherit from MTK_PROJECT_CONFIG
+MTK_PROJECT_CONFIG ?= device/nb/vernee_M5/configs/ProjectConfig.mk)
+include $(MTK_PROJECT_CONFIG)
+
+# Charger and USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.usb.vid=2970
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Init
+PRODUCT_COPY_FILES += \ #TODO DATEIEN DURCH ORIGINALE ERSETZEN
+    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
 # Audio
 $(call inherit-product, device/nb/vernee_M5/configs/audio/audio_param/audio.mk)
 
@@ -33,3 +58,46 @@ $(call inherit-product, device/nb/vernee_M5/bluetooth/bluetooth.mk)
 
 # Carrier configs
 $(call inherit-product, device/nb/vernee_M5/configs/carrier/carrier.mk)
+
+# Media
+$(call inherit-product, device/nb/vernee_M5/media/media.mk)
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
+
+# GPS
+PRODUCT_PACKAGES += \
+    YGPS
+
+# Telephony
+SIM_COUNT := 2
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
+PRODUCT_PROPERTY_OVERRIDES += persist.radio.default.sim=0
+PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Dalvik configurations
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
+
+# Versioning
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mediatek.version.release=$(MTK_BUILD_VERNO) \
+    ro.mediatek.chip_ver=$(MTK_CHIP_VER)
+
+# Inherit the rest from mt6755-common
+$(call inherit-product, device/nb/vernee_M5/mt6755.mk)
---
+++ media/media.mk
@@ -0,0 +1,8 @@
+# Copy media configuration files
+PRODUCT_COPY_FILES += \
+    device/nb/vernee_M5/media/media_codecs.xml:/system/etc/media_codecs.xml \
+    device/nb/vernee_M5/media/media_codecs_google_audio.xml:/system/etc/media_codecs_google_audio.xml \
+    device/nb/vernee_M5/media/media_codecs_google_video_le.xml:/system/etc/media_codecs_google_video_le.xml \
+    device/nb/vernee_M5/media/media_codecs_mediatek_audio.xml:/system/etc/media_codecs_mediatek_audio.xml \
+    device/nb/vernee_M5/media/media_codecs_mediatek_video.xml:/system/etc/media_codecs_mediatek_video.xml \
+    device/nb/vernee_M5/media/media_codecs_performance.xml:/system/etc/media_codecs_performance.xml
