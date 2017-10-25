2e28ff867dde9e15e3d7d5923d67af14286ab05c
Alberto Guanti <albertoguanti@gmail.com>
2017-10-25 23:58:02 +0200

    mt6755-common: cmhw: Simplify DisplayColorCalibration return function

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -16,16 +16,13 @@
 
 package org.cyanogenmod.hardware;
 
-import java.io.File;
-import java.util.Scanner;
 import org.cyanogenmod.internal.util.FileUtils;
 
 public class DisplayColorCalibration {
     private static final String COLOR_FILE = "/sys/devices/platform/mtk_disp_mgr.0/rgb";
 
     public static boolean isSupported() {
-        File f = new File(COLOR_FILE);
-        return f.exists();
+        return FileUtils.isFileWritable(COLOR_FILE);
     }
 
     public static int getMaxValue()  {
