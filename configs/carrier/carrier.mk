c0b552266a89a82016846a07e306c1ae12cadbd8
Tobias Tefke <tobias.tefke@gmail.com>
2017-12-15 20:13:55 +0100

    Do not copy APN list
    It is already included by the LineageOS build system

--- configs/carrier/carrier.mk
+++ configs/carrier/carrier.mk
@@ -1,6 +1,5 @@
 # Copy carrier configuration files
 PRODUCT_COPY_FILES += \
-    device/nb/vernee_M5/configs/carrier/apns-conf.xml:system/etc/apns-conf.xml \
     device/nb/vernee_M5/configs/carrier/spn-conf.xml:system/etc/spn-conf.xml \
     device/nb/vernee_M5/configs/carrier/cdma_ecc_list.xml:system/vendor/etc/cdma_ecc_list.xml \
     device/nb/vernee_M5/configs/carrier/cdma_ecc_list_ss.xml:system/vendor/etc/cdma_ecc_list_ss.xml \
