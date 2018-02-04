1565e39bd86a66ccab7e2c9528f650d8d54e2180
Tobias Tefke <tobias.tefke@gmail.com>
2018-02-04 18:53:53 +0100

    Replace LOCAL_PATH with DEVICE_PATH
    This variable is already used by the build system and can therefore cause issues

--- device.mk
+++ device.mk
@@ -14,11 +14,11 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/nb/vernee_M5
+DEVICE_PATH := device/nb/vernee_M5
 
 # Prebuilt kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := $(LOCAL_PATH)/boot.img-kernel
+LOCAL_KERNEL := $(DEVICE_PATH)/boot.img-kernel
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -30,11 +30,11 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product-if-exists, vendor/nb/vernee_M5/device-vendor.mk)
 
 # Inherit from MTK_PROJECT_CONFIG
-MTK_PROJECT_CONFIG ?= $(LOCAL_PATH)/ProjectConfig.mk
+MTK_PROJECT_CONFIG ?= $(DEVICE_PATH)/ProjectConfig.mk
 include $(MTK_PROJECT_CONFIG)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -64,14 +64,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.managed_users.xml:system/etc/permissions/android.software.managed_users.xml \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.webview.xml:system/etc/permissions/android.software.webview.xml \
-    $(LOCAL_PATH)/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
-    $(LOCAL_PATH)/permissions/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
-    $(LOCAL_PATH)/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-    $(LOCAL_PATH)/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    $(LOCAL_PATH)/permissions/com.android.location.provider.xml:system/etc/permissions/com.android.location.provider.xml \
-    $(LOCAL_PATH)/permissions/com.android.media.remotedisplay.xml:system/etc/permissions/com.android.media.remotedisplay.xml \
-    $(LOCAL_PATH)/permissions/com.android.mediadrm.signer.xml:system/etc/permissions/com.android.mediadrm.signer.xml \
-    $(LOCAL_PATH)/permissions/platform.xml:system/etc/permissions/platform.xml
+    $(DEVICE_PATH)/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
+    $(DEVICE_PATH)/permissions/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    $(DEVICE_PATH)/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    $(DEVICE_PATH)/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    $(DEVICE_PATH)/permissions/com.android.location.provider.xml:system/etc/permissions/com.android.location.provider.xml \
+    $(DEVICE_PATH)/permissions/com.android.media.remotedisplay.xml:system/etc/permissions/com.android.media.remotedisplay.xml \
+    $(DEVICE_PATH)/permissions/com.android.mediadrm.signer.xml:system/etc/permissions/com.android.mediadrm.signer.xml \
+    $(DEVICE_PATH)/permissions/platform.xml:system/etc/permissions/platform.xml
 
 # default.prop overrides
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -87,22 +87,22 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.bootimage.build.fingerprint=vernee/vernee_M5/vernee_M5:7.0/vernee_M5/C0011061821:user/release-keys
 
 # Audio
-$(call inherit-product, $(LOCAL_PATH)/configs/audio/audio_param/audio.mk)
+$(call inherit-product, $(DEVICE_PATH)/configs/audio/audio_param/audio.mk)
 
 # Bluetooth
-$(call inherit-product, $(LOCAL_PATH)/bluetooth/bluetooth.mk)
+$(call inherit-product, $(DEVICE_PATH)/bluetooth/bluetooth.mk)
 
 # Carrier configs
-$(call inherit-product, $(LOCAL_PATH)/configs/carrier/carrier.mk)
+$(call inherit-product, $(DEVICE_PATH)/configs/carrier/carrier.mk)
 
 # Media
-$(call inherit-product, $(LOCAL_PATH)/media/media.mk)
+$(call inherit-product, $(DEVICE_PATH)/media/media.mk)
 
 # Internet access
-$(call inherit-product, $(LOCAL_PATH)/ip/ip.mk)
+$(call inherit-product, $(DEVICE_PATH)/ip/ip.mk)
 
 # Other configuration files
-$(call inherit-product, $(LOCAL_PATH)/configs/configs.mk)
+$(call inherit-product, $(DEVICE_PATH)/configs/configs.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -137,4 +137,4 @@ $(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.m
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
 
 # Inherit the rest from mt6755-common
-$(call inherit-product, $(LOCAL_PATH)/mt6755.mk)
+$(call inherit-product, $(DEVICE_PATH)/mt6755.mk)
