baf75ad318d344ee4dc69a9aad21bf9ad0946467
Tobias Tefke <tobias.tefke@gmail.com>
2018-02-04 18:57:29 +0100

    Forcefully include proprietary blobs

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 
-$(call inherit-product-if-exists, vendor/nb/vernee_M5/device-vendor.mk)
+$(call inherit-product, vendor/nb/vernee_M5/device-vendor.mk)
 
 # Inherit from MTK_PROJECT_CONFIG
 MTK_PROJECT_CONFIG ?= $(DEVICE_PATH)/ProjectConfig.mk
