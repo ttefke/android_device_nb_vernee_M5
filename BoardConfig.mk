1d91bf3a1e2e1a16572020e246412fbbeb843f81
Tobias Tefke <tobias.tefke@gmail.com>
2018-02-09 14:07:49 +0100

    Set selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,8 +73,8 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 
-BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
-BOARD_KERNEL_CMDLINE += androidboot.selinux=disabled
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_MKBOOTIMG_ARGS := --board "1509963705" --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --tags_offset 0x03f88000
 
