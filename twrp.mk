f3b20f4f3a4f1f09ba0e32b233cc898cc268a0a5
Tobias Tefke <tobias.tefke@gmail.com>
2018-03-04 18:38:38 +0100

    [TEMPORARY] Add TWRP event logging

--- twrp.mk
+++ twrp.mk
@@ -37,3 +37,4 @@ TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := false
 TW_DEFAULT_LANGUAGE := en-US 
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/recovery/etc/twrp.fstab
+TW_EVENT_LOGGING := true # Temporary
