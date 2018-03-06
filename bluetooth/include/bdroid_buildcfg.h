9830932b333260e174ec67618165bb93009466b9
Tobias Tefke <tobias.tefke@gmail.com>
2018-03-06 13:45:20 +0100

    Update copyright headers

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2018 Tobias Tefke
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright (C) 2018 Tobias Tefke
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
