99a0538bf9fbd09ae5874aa6084815d7536f4b5c
Tobias Tefke <tobias.tefke@gmail.com>
2018-03-25 15:30:53 +0200

    Add FM radio configuration file

--- configs/configs.mk
+++ configs/configs.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
     device/nb/vernee_M5/configs/mediaextractor-seccomp.policy:system/etc/seccomp_policy/mediaextractor-seccomp.policy \
     device/nb/vernee_M5/configs/aee-config:system/vendor/etc/aee-config \
     device/nb/vernee_M5/configs/factory.ini:system/vendor/etc/factory.ini \
+    device/nb/vernee_M5/configs/fm_cust.cfg:system/vendor/firmware/fm_cust.cfg \
     device/nb/vernee_M5/configs/mtklog-config.prop:system/vendor/etc/mtklog-config.prop \
     device/nb/vernee_M5/configs/partition_permission.sh:system/vendor/etc/partition_permission.sh \
     device/nb/vernee_M5/configs/perfcontable.txt:system/vendor/etc/perfcontable.txt \
