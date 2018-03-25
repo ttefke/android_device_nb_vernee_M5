e7a3c1c4da7055768de5d6624a9333d7df17ca30
Tobias Tefke <tobias.tefke@gmail.com>
2018-03-25 19:31:56 +0200

    Cleanup media configuration files

--- media/media.mk
+++ media/media.mk
@@ -15,9 +15,11 @@
 
 # Copy media configuration files
 PRODUCT_COPY_FILES += \
+    device/nb/vernee_M5/media/mtk_omx_core.cfg:system/vendor/etc/mtk_omx_core.cfg \
     device/nb/vernee_M5/media/media_codecs.xml:system/etc/media_codecs.xml \
     device/nb/vernee_M5/media/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     device/nb/vernee_M5/media/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     device/nb/vernee_M5/media/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
     device/nb/vernee_M5/media/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
-    device/nb/vernee_M5/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    device/nb/vernee_M5/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    device/nb/vernee_M5/media/media_profiles.xml:system/etc/media_profiles.xml
