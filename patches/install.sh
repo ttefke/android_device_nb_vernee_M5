d949c557955a310a98e1a1d5449b6bbefcb5cce6
Tobias Tefke <tobias.tefke@gmail.com>
2018-04-01 18:05:39 +0200

    Add initial vold patches

--- patches/install.sh
+++ patches/install.sh
@@ -42,6 +42,16 @@ for dir in $dirs ; do
 	echo " "
 done
 
+dirs="system/vold"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git am $rootdirectory/device/nb/vernee_M5/patches/$dir/*.patch
+	echo " "
+done
+
 # -----------------------------------
 echo "Changing to build directory..."
 cd $rootdirectory
