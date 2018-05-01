790eba4acbb1bcc3e1d846d1556cefdfd5a4429e
Tobias Tefke <tobias.tefke@gmail.com>
2018-05-01 19:15:01 +0200

    Properly shim program_binary_builder
    Change-Id: Icce4c6ca0314c949c5dfe76c1595b1fc655177bb

--- libshims/program_binary_builder.cpp
+++ libshims/program_binary_builder.cpp
@@ -15,8 +15,16 @@
  */
 
 extern "C" {
-    void _ZN7android10uirenderer12ProgramAtlas14createProgramsEPlPi() {}
-    void _ZN7android10uirenderer12ProgramAtlas28loadProgramBinariesAndDeleteEPliPvi() {}
-    void _ZN7android10uirenderer12ProgramAtlas9terminateEv() {}
+    int _ZN7android10uirenderer12ProgramAtlas14createProgramsEPlPi() {
+        return 0;
+    }
+
+    int _ZN7android10uirenderer12ProgramAtlas28loadProgramBinariesAndDeleteEPliPvi() {
+        return 0;
+    }
+
+    int _ZN7android10uirenderer12ProgramAtlas9terminateEv() {
+        return 0;
+    }
 }
  
