61c6d2e06e445284fa84dbea6e668481c93b9f06
Tobias Tefke <tobias.tefke@gmail.com>
2018-05-07 15:27:20 +0200

    Add HIDL

---
+++ hidl.mk
@@ -0,0 +1,236 @@
+#
+# Copyright (C) 2018 Tobias Tefke
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-impl \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-impl \
+    android.hardware.configstore@1.0-service
+
+# Dumpstate
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-impl \
+    android.hardware.dumpstate@1.0-service
+
+# Gralloc
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service
+
+# Bufferqueue
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.bufferqueue@1.0-impl \
+    android.hardware.graphics.bufferqueue@1.0-service
+
+# Graphics common
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.common@1.0-impl \
+    android.hardware.graphics.common@1.0-service
+
+# Graphics composer
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
+
+# Graphics mapper
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@1.0-impl
+
+# Renderscript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0-impl \
+    android.hardware.radio@1.0-service
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-impl \
+    android.hardware.usb@1.0-service
+
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-impl \
+    android.hardware.wifi@1.0-service
+
+# Offload
+PRODUCT_PACKAGES += \
+    android.hardware.wifi.offload@1.0-impl \
+    android.hardware.wifi.offload@1.0-service
+
+# Supplicant
+PRODUCT_PACKAGES += \
+    android.hardware.wifi.supplicant@1.0-impl \
+    android.hardware.wifi.supplicant@1.0-service
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.wifi.keystore@1.0-impl \
+    android.system.wifi.keystore@1.0-service
+
+# Netd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0-impl \
+    android.system.net.netd@1.0-service
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.common@2.0-impl \
+    android.hardware.audio.common@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
+# Boot
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+# FM radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.broadcastradio@1.0-service
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0-impl \
+    android.hardware.camera.common@1.0-service \
+    android.hardware.camera.device@3.3-impl \
+    android.hardware.camera.device@3.3-service \
+    android.hardware.camera.metadata@3.2-impl \
+    android.hardware.camera.metadata@3.2-service \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
+
+# CAS
+PRODUCT_PACKAGES += \
+    android.hardware.cas@1.0-impl \
+    android.hardware.cas@1.0-service \
+    android.hardware.cas.native@1.0-impl \
+    android.hardware.cas.native@1.0-service
+
+# Contexthub
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl \
+    android.hardware.contexthub@1.0-service
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
+# Infrared
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
+# Media
+PRODUCT_PACKAGES += \
+    amdroid.hardware.media@1.0-impl \
+    android.hardware.media@1.0-service
+
+# Media: OMX support
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media@1.0-service
+
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.medmtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+# Minijail
+PRODUCT_PACKAGES += \
+    libhwminijail
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
+# Sound
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
+
+# Tethering
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0-impl \
+    android.hardware.tetheroffload.config@1.0-service \
+    android.hardware.tetheroffload.control@1.0-impl \
+    android.hardware.tetheroffload.control@1.0-service
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+# Weaver
+PRODUCT_PACKAGES += \
+    android.hardware.weaver@1.0-impl \
+    android.hardware.weaver@1.0-service
+
+# VR
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-impl \
+    android.hardware.vr@1.0-service
