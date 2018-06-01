56a37f3b48e1328d53eaf64c3f13d06efa748066
Tobias Tefke <tobias.tefke@tutanota.com>
2018-06-01 18:24:03 +0200

    Revert "Enable small fonts"
    Some MTK apps required this patch.
    They were removed in commit 997cf6a3e29cdb81d776637763fe1d79014b5a13.
    
    Moreover the small fonts do not have full UTF-8 support.
    This results in black boxes being shown instead of emojis.
    
    Therefore this patch should be reverted.
    
    This reverts commit ded7500d418f9ae0015a68b40f96ac8d82cc21ff.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,9 +111,6 @@ PRESENT_TIME_OFFSET_FROM_VSYNC_NS := 0
 MTK_HWC_SUPPORT := yes
 MTK_HWC_VERSION := 1.5.0
 
-# Small fonts
-SMALLER_FONT_FOOTPRINT := true
-
 # Override renderscript driver
 OVERRIDE_RS_DRIVER := libRSDriver_mtk.so
 
